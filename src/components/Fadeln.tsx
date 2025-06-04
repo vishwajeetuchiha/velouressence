6d7df79f0257ce1d8e07ddb69cc867b35f031486
vishwajeetuchiha <vishurao7048@gmail.com>
2025-06-04 12:58:04 +0530

    initial commit

---
+++ src/components/Fadeln.tsx
@@ -0,0 +1,74 @@
+"use client"
+import { useGSAP } from "@gsap/react";
+import clsx from "clsx";
+import {gsap} from "gsap"
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useRef } from "react";
+
+gsap.registerPlugin(useGSAP, ScrollTrigger)
+
+type FadeInProps = {
+    children: React.ReactNode;
+    vars?: gsap.TweenVars;
+    className?: string;
+    start?: string
+}
+
+export const FadeIn = ({
+   children ,
+    start="top 80%" ,
+     vars = {} , className}: FadeInProps) => {
+
+    const containerRef = useRef<HTMLDivElement>(null);
+
+    useGSAP(
+      () => {
+
+
+        const mm = gsap.matchMedia()
+
+        mm.add("(prefers-reduced-motion: no-preference)", ()=> {
+          gsap.to(containerRef.current , {
+            duration: 5,
+            opacity: 1,
+            ease: "power3.out",
+            y:0,
+            ...vars,
+            scrollTrigger: {
+              trigger: containerRef.current,
+               start,
+            }
+          })
+        })
+
+        mm.add("(prefers-reduced-motion: reduce)", () => {
+          gsap.to(containerRef.current , {
+            duration: 0.5,
+            opacity: 1,
+            ease: "none",
+            y:0,
+            stagger:0
+            
+          })
+        })
+      }
+    )
+    
+    useGSAP(
+      () => {
+        gsap.to(containerRef.current, {
+          duration: 5,
+          opacity: 1 ,
+          ease: "power3.out",
+          y:0,
+          ...vars,
+        })
+      }, {scope: containerRef,}
+    )
+    
+    return (
+        <div ref={containerRef} className={clsx("opacity-0", className)}>
+            {children}
+        </div>
+    )
+}
---
+++ src/slices/FragranceList/FragranceDisplay.tsx
@@ -0,0 +1,71 @@
+import { ButtonLink } from "@/components/ButtonLink";
+import { FadeIn } from "@/components/Fadeln";
+// import { FadeIn } from "@/components/FadeIn";
+import { FragranceAttributes } from "@/components/FragranceAttributes";
+import { createClient } from "@/prismicio";
+import { Content } from "@prismicio/client";
+import { PrismicNextImage } from "@prismicio/next";
+import { PrismicRichText, PrismicText } from "@prismicio/react";
+import { HiPlus } from "react-icons/hi2";
+
+type FragranceDisplayProps = {
+  id: string;
+};
+
+export const FragranceDisplay = async ({ id }: FragranceDisplayProps) => {
+  const client = createClient();
+  const fragrance = await client.getByID<Content.FragranceDocument>(id);
+
+  return (
+    <FadeIn
+      className="relative z-10 grid min-h-[85vh] w-full translate-y-20 items-center justify-items-start border border-white/10 p-4 text-left md:p-14 lg:p-20"
+      vars={{ duration: 2.5 }}
+      start="top 50%"
+    >
+      <div className="absolute inset-0 z-0">
+        <PrismicNextImage
+          field={fragrance.data.feature_image}
+          className="object-cover opacity-40 md:opacity-100"
+          fill
+          width={1150}
+          quality={90}
+          alt=""
+        />
+      </div>
+
+      <FadeIn
+        className="relative z-10 grid translate-y-8"
+        vars={{ duration: 3, delay: 0.8 }}
+        start="top 50%"
+      >
+        <h3 className="font-display mb-3 text-5xl md:text-6xl lg:text-7xl">
+          <PrismicText field={fragrance.data.title} />
+        </h3>
+
+        <p className="mb-8 text-base font-semibold text-gray-300">
+          Eau de Parfum
+        </p>
+
+        <div className="mb-10 max-w-md text-lg text-gray-300">
+          <PrismicRichText field={fragrance.data.description} />
+        </div>
+
+        <FragranceAttributes
+          scentProfile={fragrance.data.scent_profile}
+          mood={fragrance.data.mood}
+          className="mb-10"
+        />
+
+        <div className="flex flex-wrap gap-4">
+          <ButtonLink document={fragrance} variant="Secondary">
+            Discover
+          </ButtonLink>
+
+          <ButtonLink href="#" variant="Primary">
+            <HiPlus className="mr-2" /> <span>Add to bag</span>
+          </ButtonLink>
+        </div>
+      </FadeIn>
+    </FadeIn>
+  );
+};
