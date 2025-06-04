ac3698872e657c16a23f111e3ec7cd35d09c0f86
vishwajeetuchiha <vishurao7048@gmail.com>
2025-06-04 17:59:15 +0530

    scrolltext

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  eslist:{
-    ignoreDuringBuilds: true
-  },
+ 
 };
 
 export default nextConfig;
